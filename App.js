c48fa3c0b4eed1a694b55fc87e948a847a878cc2
Akshay Jain <jainakshay1783@gmail.com>
2024-01-29 17:09:33 -0800

    Ep. 02 Igniting our App (Notes + HW)

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "HMR (Hot Module Replacement)"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namaste-React
+
+# Parcel
+
+    - Dev Build
+    - Host on Local Server
+    - HMR (Hot Module Replacement)
+    - File Watching Algorithm (in C++)
+    - Caching for Faster Build time
+    - Image Optimization
+    - Minification for production build
+    - Bundling
+    - Compressing (remove whitespaces)
+    - Content Hashing
+    - Differential Bundling (support older browsers)
+    - Beautiful Diagnostic
+    - Host on HTTPS as well
+    - Tree Shaking (remove unused code)
